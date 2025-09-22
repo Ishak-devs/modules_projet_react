b2b8964f7d91dd5b92d00457c857618d786d00c3
ishak-devs <kouicicontact@yahoo.com>
2025-09-22 12:18:27 +0200

    function todo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import ClassState from './Components/ClassState';
 import FunctionState from './Components/FunctionState';
+import Todo from './Components/Todo';
 
 function App() {
   return (
     <div>
-    
+    <Todo/>
     </div>
   );
 }
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Todo extends Component {
+const Todo = () => {
+  const [todos, setTodos] =  useState([
+    { id: 1, todos: 'hello world'},
+    { id: 2, todos: 'hello world2'},
+    { id: 3, todos: 'hello world3'}
+   ])
 
-    Todo=() => {
-            
-             }
-    }
+   console.log(todos.length);
 
+  return (
+    <div>{todos.legth}</div>
+  )
+}
 
-    return (
-
-      <div>
-     
-      </div>
-    )
-  
-
-export default Todo;
+export default Todo
