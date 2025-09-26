a575b33916381809f9153693ac532feeb6587d65
ishak-devs <kouicicontact@yahoo.com>
2025-09-26 12:12:39 +0200

    ajout de fonction

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
+import { useState } from 'react';
 import './App.css';
 
 
+
 function App () {
-                    return (
-                    <div>
+            const [livre, setlivre] = useState('');
+            const [auteur, setauteur] = useState('');
+
+
+             const Valider_form = (e) => {
+              e.preventDefault(); 
+             
+    console.log('Livre :', livre);
+    console.log('Auteur :', auteur);
+
+    setauteur ('');
+    setlivre ('');
+             }
+
+     return (
+      <div>
 
                       <h1>BOOKS</h1>
                       <p>Ajouter un livre à votre bibliothèque</p>
-                   <input type= 'text'   placeholder="livre"></input>
+                      <form id='formulaire' onSubmit={Valider_form}>
+
+                
+                   <input type= 'text'   placeholder="livre" id='livre' onChange={(e) => setlivre(e.target.value)}></input>
                   
-                   <input type= 'text' placeholder='auteur'></input>
+                   <input type= 'text' placeholder='auteur' id='auteur' onChange={(e) => setauteur (e.target.value)}></input>
 
                    <button type='submit'>Ajouter un livre</button>
+                  
+                    </form>
                         </div>
-                    )
-                        
-    }
+     )
+                     
+
+  }
+   
   
 
 export default App;
