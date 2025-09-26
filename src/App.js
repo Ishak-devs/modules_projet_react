8a776a9222d826a2d0ae950a4109b0fe4fb70e30
ishak-devs <kouicicontact@yahoo.com>
2025-09-26 11:37:29 +0200

    debut books

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import ClassState from './Components/ClassState';
-import FunctionState from './Components/FunctionState';
-import Todo from './Components/Todo';
 
-function App() {
-  return (
-    <div>
-    <Todo/>
-    </div>
-  );
-}
+
+function App () {
+                    return (
+                    <div>
+
+                      <h1>BOOKS</h1>
+                      <p>Ajouter un livre à votre bibliothèque</p>
+                   <input type= 'text'   placeholder="livre"></input>
+                  
+                   <input type= 'text' placeholder='auteur'></input>
+
+                   <button type='submit'>Ajouter un livre</button>
+                        </div>
+                    )
+                        
+    }
+  
 
 export default App;
